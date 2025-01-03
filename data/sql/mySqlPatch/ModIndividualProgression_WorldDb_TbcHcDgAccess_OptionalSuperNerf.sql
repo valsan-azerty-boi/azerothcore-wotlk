6fe5f8d8a389822fc2f510aa46b5c5b16fe0bfb3
valsan-azerty-boi <52854501+valsan-azerty-boi@users.noreply.github.com>
2025-01-03 19:18:22 +0100

    Create ModIndividualProgression_WorldDb_TbcHcDgAccess_OptionalSuperNerf.sql

---
+++ data/sql/mySqlPatch/ModIndividualProgression_WorldDb_TbcHcDgAccess_OptionalSuperNerf.sql
@@ -0,0 +1,10 @@
+/* 
+TBC hc dungeon attunement => Super Nerf (Friendly)
+https://github.com/ZhengPeiRu21/mod-individual-progression)
+*/
+UPDATE `item_template` SET `RequiredReputationRank` = 4 WHERE (`entry` = 30622);
+UPDATE `item_template` SET `RequiredReputationRank` = 4 WHERE (`entry` = 30637);
+UPDATE `item_template` SET `RequiredReputationRank` = 4 WHERE (`entry` = 30633);
+UPDATE `item_template` SET `RequiredReputationRank` = 4 WHERE (`entry` = 30634);
+UPDATE `item_template` SET `RequiredReputationRank` = 4 WHERE (`entry` = 30623);
+UPDATE `item_template` SET `RequiredReputationRank` = 4 WHERE (`entry` = 30635);
