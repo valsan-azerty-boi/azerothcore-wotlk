c19ab79934c9830bac711b88d1c8c7b9d9bc8fc5
valsan-azerty-boi <52854501+valsan-azerty-boi@users.noreply.github.com>
2025-01-03 19:19:12 +0100

    Create ModIndividualProgression_WorldDb_TbcHcDgAccess_RestoreValues.sql

---
+++ data/sql/mySqlPatch/ModIndividualProgression_WorldDb_TbcHcDgAccess_RestoreValues.sql
@@ -0,0 +1,10 @@
+/* 
+TBC hc dungeon attunement => Restore original values 
+https://github.com/ZhengPeiRu21/mod-individual-progression
+*/
+UPDATE `item_template` SET `RequiredReputationRank` = 6 WHERE (`entry` = 30622);
+UPDATE `item_template` SET `RequiredReputationRank` = 6 WHERE (`entry` = 30637);
+UPDATE `item_template` SET `RequiredReputationRank` = 6 WHERE (`entry` = 30633);
+UPDATE `item_template` SET `RequiredReputationRank` = 6 WHERE (`entry` = 30634);
+UPDATE `item_template` SET `RequiredReputationRank` = 6 WHERE (`entry` = 30623);
+UPDATE `item_template` SET `RequiredReputationRank` = 6 WHERE (`entry` = 30635);
