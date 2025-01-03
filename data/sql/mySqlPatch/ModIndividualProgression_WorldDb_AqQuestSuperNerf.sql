752b5ae02b8bb1908beffd0158e06aee765f61cf
valsan-azerty-boi <52854501+valsan-azerty-boi@users.noreply.github.com>
2025-01-03 18:17:32 +0100

    Update ModIndividualProgression_WorldDb_AqQuestSuperNerf.sql

--- data/sql/mySqlPatch/ModIndividualProgression_WorldDb_AqQuestSuperNerf.sql
+++ data/sql/mySqlPatch/ModIndividualProgression_WorldDb_AqQuestSuperNerf.sql
@@ -2,5 +2,5 @@
    This reduces the quest requirements to be more reasonably completable by a solo player or small group.
    Be sure to run AFTER vanilla_quest_reputations.sql. (https://github.com/ZhengPeiRu21/mod-individual-progression)
    Increases the Rep for The Hand of the Righteous, so only about 1 turn-in is necessary instead of 200 (super nerf). */
-UPDATE quest_template SET RewardFactionOverride1=4050000, RewardFactionOverride2=0,RewardFactionOverride3=0,RewardFactionOverride4=0,RewardFactionOverride5=0 WHERE ID=8301;
-UPDATE quest_template SET RewardFactionOverride1=4050000, RewardFactionOverride2=0,RewardFactionOverride3=0,RewardFactionOverride4=0,RewardFactionOverride5=0 WHERE ID=8302;
+UPDATE quest_template SET RewardFactionOverride1=4160100, RewardFactionOverride2=0,RewardFactionOverride3=0,RewardFactionOverride4=0,RewardFactionOverride5=0 WHERE ID=8301;
+UPDATE quest_template SET RewardFactionOverride1=4160100, RewardFactionOverride2=0,RewardFactionOverride3=0,RewardFactionOverride4=0,RewardFactionOverride5=0 WHERE ID=8302;
