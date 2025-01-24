7073e2cb665cf2ffdfcf0f4359aa3cd8e4b96992
valsan-azerty-boi <52854501+valsan-azerty-boi@users.noreply.github.com>
2025-01-24 16:51:25 +0100

    Create ModAhBot_WorldDb_ItemNumber.sql

---
+++ _myPatch/Sql/ModAhBot_WorldDb_ItemNumber.sql
@@ -0,0 +1,3 @@
+UPDATE mod_auctionhousebot SET minitems = 10000, maxitems = 10500 WHERE name = 'Alliance';
+UPDATE mod_auctionhousebot SET minitems = 10000, maxitems = 10500 WHERE name = 'Horde';
+UPDATE mod_auctionhousebot SET minitems = 1500, maxitems = 2000 WHERE name = 'Neutral';
