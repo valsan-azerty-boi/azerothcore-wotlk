acba4fb33bea6a5ce2bc549f874defc36a2eaf12
valsan-azerty-boi <52854501+valsan-azerty-boi@users.noreply.github.com>
2025-03-07 00:33:18 +0100

    Add files via upload

---
+++ _myPatch/Sql/mod-assistant_custom-for-aq-gong-event.sql
@@ -0,0 +1,18 @@
+UPDATE `item_template` SET `BuyPrice` = 50000 WHERE `entry` = 20384 AND `BuyPrice` = 0;
+UPDATE `item_template` SET `BuyPrice` = 50000 WHERE `entry` = 12360 AND `BuyPrice` = 0;
+UPDATE `item_template` SET `BuyPrice` = 400000 WHERE `entry` = 18562 AND `BuyPrice` = 0;
+UPDATE `item_template` SET `BuyPrice` = 40000 WHERE `entry` = 12800 AND `BuyPrice` = 0;
+UPDATE `item_template` SET `BuyPrice` = 28000 WHERE `entry` = 12361 AND `BuyPrice` = 0;
+UPDATE `item_template` SET `BuyPrice` = 1000 WHERE `entry` = 9061 AND `BuyPrice` = 0;
+UPDATE `item_template` SET `BuyPrice` = 1000 WHERE `entry` = 8150 AND `BuyPrice` = 0;
+UPDATE `item_template` SET `BuyPrice` = 10000 WHERE `entry` = 21024 AND `BuyPrice` = 0;
+
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES
+(9000045, 20384), -- Silithid Carapace Fragment (Container)
+(9000023, 12360), -- Arcanite Bar (Gem: Meta)
+(9000025, 18562), -- Elementium Ore (Gem: Blue)
+(9000023, 12800), -- Azerothian Diamond (Gem: Meta)
+(9000025, 12361), -- Blue Sapphire (Gem: Blue)
+(9000045, 9061), -- Goblin Rocket Fuel (Container)
+(9000033, 8150), -- Deeprock Salt (Food)
+(9000033, 21024); -- Chimaerok Tenderloin (Food)
