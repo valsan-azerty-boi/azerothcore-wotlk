c5c68c952104fa039f2156fb5116e43b7fdd04e0
valsan-azerty-boi <52854501+valsan-azerty-boi@users.noreply.github.com>
2025-04-17 16:30:32 +0200

    Update spectral_tiger_custo.sql

--- _myPatch/Sql/spectral_tiger_custo.sql
+++ _myPatch/Sql/spectral_tiger_custo.sql
@@ -1,1 +1,3 @@
 UPDATE `item_template` SET `BuyPrice` = 10000000 WHERE (`entry` = 33225); -- buy price 1000 gold (spectral tiger fast)
+UPDATE `item_template` SET `BuyPrice` = 10000000 WHERE (`entry` = 49284); -- buy price 1000 gold (spectral tiger fast)
+UPDATE `item_template` SET `BuyPrice` = 1000000 WHERE (`entry` = 49343); -- spectral pet
