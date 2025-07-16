bfccbf8eb578215cf197f1edc69b0f6ed6912a4d
valsan-azerty-boi <52854501+valsan-azerty-boi@users.noreply.github.com>
2025-07-16 15:46:08 +0200

    Update general.sql

--- _myPatch/Sql/general.sql
+++ _myPatch/Sql/general.sql
@@ -2,3 +2,7 @@ UPDATE `command` SET `security` = '1' WHERE `name` = 'commands';
 UPDATE `command` SET `security` = '1' WHERE `name` = 'help';
 
 
+UPDATE `creature_template` SET `name` = 'Tsuridormi', `subname` = 'Timewalking Master' WHERE `entry` = '9000000';
+INSERT IGNORE INTO `creature_template_locale` (`entry`, `locale`, `Name`, `Title`, `VerifiedBuild`) VALUES ('9000000', 'frFR', 'Tsuridormi', 'Maître des Marcheurs du temps', '0');
+DELETE FROM creature WHERE `entry` = '9000000';
+
